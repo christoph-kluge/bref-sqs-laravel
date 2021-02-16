62890e05338c36085953822a4e06244bf08f21a6
korridor <26689068+korridor@users.noreply.github.com>
2021-02-16 12:28:54 +0100

    Laravel 8 support

--- src/Commands/SqsWorkCommand.php
+++ src/Commands/SqsWorkCommand.php
@@ -11,18 +11,22 @@ use Sikei\Bref\Sqs\Laravel\Queue\Queue;
 class SqsWorkCommand extends WorkCommand
 {
 
-    protected $signature = 'sqs:work
+	protected $signature = 'sqs:work
                             {connection? : The name of the queue connection to work}
+                            {--name=default : The name of the worker}
                             {--queue= : The names of the queues to work}
                             {--daemon : Run the worker in daemon mode (Deprecated)}
                             {--once : Only process the next job on the queue}
                             {--stop-when-empty : Stop when the queue is empty}
-                            {--delay=0 : The number of seconds to delay failed jobs}
+                            {--delay=0 : The number of seconds to delay failed jobs (Deprecated)}
+                            {--backoff=0 : The number of seconds to wait before retrying a job that encountered an uncaught exception}
+                            {--max-jobs=0 : The number of jobs to process before stopping}
+                            {--max-time=0 : The maximum number of seconds the worker should run}
                             {--force : Force the worker to run even in maintenance mode}
                             {--memory=128 : The memory limit in megabytes}
                             {--sleep=3 : Number of seconds to sleep when no job is available}
                             {--timeout=60 : The number of seconds a child process can run}
-                            {--tries=0 : Number of times to attempt a job before logging it failed}';
+                            {--tries=1 : Number of times to attempt a job before logging it failed}';
 
     /** @var LambdaRuntime */
     protected $lambdaRuntime;
@@ -56,6 +60,7 @@ class SqsWorkCommand extends WorkCommand
     protected function runWorker($connection, $queueName)
     {
         $this->worker->setCache($this->cache);
+        $this->worker->setName($this->option('name'));
 
         /** @var Queue $queue */
         $queue = $this->worker->getManager()->connection($connection);
