7853addf3e415bf793eb3d21017bb5a81ade5d0d
korridor <26689068+korridor@users.noreply.github.com>
2021-03-28 20:13:43 -0500

    Added rest option to SqsWorkCommand for laravel version 8.32.0

--- src/Commands/SqsWorkCommand.php
+++ src/Commands/SqsWorkCommand.php
@@ -25,6 +25,7 @@ class SqsWorkCommand extends WorkCommand
                             {--force : Force the worker to run even in maintenance mode}
                             {--memory=128 : The memory limit in megabytes}
                             {--sleep=3 : Number of seconds to sleep when no job is available}
+                            {--rest=0 : Number of seconds to rest between jobs}
                             {--timeout=60 : The number of seconds a child process can run}
                             {--tries=1 : Number of times to attempt a job before logging it failed}';
 
